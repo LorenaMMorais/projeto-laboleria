f42d6f968441a50646325f84d1144e70f5751fa0
Lorena Morais <lorenamorais@alunos.utfpr.edu.br>
2023-03-22 23:18:50 -0300

    feat: add postClients

--- src/controllers/clientsController.js
+++ src/controllers/clientsController.js
@@ -1,5 +1,24 @@
-export async function postClients(req, res){
+import db from "../db.js";
 
+export async function postClients(req, res){
+    const { name, address, phone } = req.body;
+    const validation = clientsSchema.validate({ name, address, phone }, { abortEarly: false });
+
+    if(validation.error){
+        const erro = validation.error.details.map((d) => d.message);
+        res.status(422).send(erro);
+        return
+    }
+
+    if(!name || !address || !phone) return res.sendStatus(400);
+
+    try {
+        await db.query(`INSERT INTO clients ("name", "address", "phone") VALUES ($1, $2, $3);`, [name, address, phone]);
+        res.sendStatus(201);
+    }catch(error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
 }
 
 export async function getClientsOrder(req, res){
--- src/index.js
+++ src/index.js
@@ -11,5 +11,5 @@ app.use(cors());
 app.use(json());
 app.use(router);
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running in port: ${port}`));
